69e7aef4d414c79a243723366f178cc0b6f7313f
Pedro-Muro-Ramirez <84997689+Pedro-Muro-Ramirez@users.noreply.github.com>
2025-10-05 19:19:51 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Initial variables
 const postForm = document.getElementById('post-form')
 const titleInput = document.getElementById('post-title')
 const bodyInput = document.getElementById('post-body')
-
 let postsArray = []
 
+// Function to render posts
 function renderPosts(){
     let postText = ``
         postsArray.forEach(function(post){
@@ -15,33 +16,39 @@ function renderPosts(){
         document.getElementById('posts-container').innerHTML = postText
 }
 
+// Fetch initial posts
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(data => {
+        // Store first 5 posts and render them
         postsArray = data.slice(0, 5)
         renderPosts()
     })
 
+// Event listener for form submission   
 postForm.addEventListener("submit", function(e){
+    // Prevent default form submission behavior
     e.preventDefault()
+    // Get input values and create new post object
     const postTitle = titleInput.value
     const postBody = bodyInput.value
-    const data = {
+    const newPost = {
         title: postTitle,
         body: postBody
     }
+    // Send POST request to API
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
         method: "POST", 
-        body: JSON.stringify(data), 
+        body: JSON.stringify(newPost), 
         headers: {
             "Content-Type": 'application/json'
         }
     })
     .then(response => response.json())
-    .then(data => {
+    .then(data => { 
+        // Add new post to the beginning of the array and re-render posts
         postsArray.unshift(data)
         renderPosts()
         postForm.reset()
     })
 })
-console.log(postsArray)
